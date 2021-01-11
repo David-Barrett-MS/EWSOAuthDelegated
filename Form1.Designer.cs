453ddfbd29fb73a0decdb04a04eeee8af485f338
David Barrett <dabarre@microsoft.com>
2021-01-11 12:08:48 +0000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace EWSOAuthDelegated
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBoxAppId = new System.Windows.Forms.TextBox();
+            this.textBoxTenantId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBoxResults = new System.Windows.Forms.TextBox();
+            this.buttonFindFolders = new System.Windows.Forms.Button();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBoxTenantId);
+            this.groupBox1.Controls.Add(this.textBoxAppId);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(327, 81);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Application Information";
+            // 
+            // textBoxAppId
+            // 
+            this.textBoxAppId.Location = new System.Drawing.Point(86, 19);
+            this.textBoxAppId.Name = "textBoxAppId";
+            this.textBoxAppId.Size = new System.Drawing.Size(235, 20);
+            this.textBoxAppId.TabIndex = 0;
+            this.textBoxAppId.Text = "74546912-2fc3-4e3d-b11a-456062729143";
+            // 
+            // textBoxTenantId
+            // 
+            this.textBoxTenantId.Location = new System.Drawing.Point(86, 45);
+            this.textBoxTenantId.Name = "textBoxTenantId";
+            this.textBoxTenantId.Size = new System.Drawing.Size(235, 20);
+            this.textBoxTenantId.TabIndex = 1;
+            this.textBoxTenantId.Text = "fc69f6a8-90cd-4047-977d-0c768925b8ec";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Application Id:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Tenant Id:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBoxResults);
+            this.groupBox2.Location = new System.Drawing.Point(12, 99);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(327, 111);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Results";
+            // 
+            // textBoxResults
+            // 
+            this.textBoxResults.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxResults.Location = new System.Drawing.Point(3, 16);
+            this.textBoxResults.Multiline = true;
+            this.textBoxResults.Name = "textBoxResults";
+            this.textBoxResults.ReadOnly = true;
+            this.textBoxResults.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxResults.Size = new System.Drawing.Size(321, 92);
+            this.textBoxResults.TabIndex = 0;
+            // 
+            // buttonFindFolders
+            // 
+            this.buttonFindFolders.Location = new System.Drawing.Point(12, 213);
+            this.buttonFindFolders.Name = "buttonFindFolders";
+            this.buttonFindFolders.Size = new System.Drawing.Size(75, 23);
+            this.buttonFindFolders.TabIndex = 2;
+            this.buttonFindFolders.Text = "Find Folders";
+            this.buttonFindFolders.UseVisualStyleBackColor = true;
+            this.buttonFindFolders.Click += new System.EventHandler(this.buttonFindFolders_Click);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(264, 216);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(75, 23);
+            this.buttonClose.TabIndex = 3;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 250);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.buttonFindFolders);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "EWS OAuth Delegated Sample (using MSAL)";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxTenantId;
+        private System.Windows.Forms.TextBox textBoxAppId;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBoxResults;
+        private System.Windows.Forms.Button buttonFindFolders;
+        private System.Windows.Forms.Button buttonClose;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using Microsoft.Exchange.WebServices.Data;
+using Microsoft.Identity.Client;
+
+namespace EWSOAuthDelegated
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void WriteToResults(string data)
+        {
+            // Add the given data to the results textbox
+
+            Action action = new Action(() => {
+                textBoxResults.AppendText($"{data}{Environment.NewLine}");
+            });
+            if (textBoxResults.InvokeRequired)
+                textBoxResults.Invoke(action);
+            else
+                action();
+        }
+
+        private void buttonFindFolders_Click(object sender, EventArgs e)
+        {
+            Action action = new Action(async () =>
+            {
+                // Configure the MSAL client to get tokens
+                var pcaOptions = new PublicClientApplicationOptions
+                {
+                    ClientId = textBoxAppId.Text,
+                    TenantId = textBoxTenantId.Text
+                };
+
+                var pca = PublicClientApplicationBuilder
+                    .CreateWithApplicationOptions(pcaOptions).Build();
+
+                var ewsScopes = new string[] { "https://outlook.office.com/EWS.AccessAsUser.All" };
+
+                try
+                {
+                    // Make the interactive token request
+                    var authResult = await pca.AcquireTokenInteractive(ewsScopes).ExecuteAsync();
+
+                    // Configure the ExchangeService with the access token
+                    var ewsClient = new ExchangeService();
+                    ewsClient.Url = new Uri("https://outlook.office365.com/EWS/Exchange.asmx");
+                    ewsClient.Credentials = new OAuthCredentials(authResult.AccessToken);
+
+                    // Make an EWS call
+                    var folders = ewsClient.FindFolders(WellKnownFolderName.MsgFolderRoot, new FolderView(10));
+                    foreach (var folder in folders)
+                    {
+                        WriteToResults($"Folder: {folder.DisplayName}");
+                    }
+                }
+                catch (MsalException ex)
+                {
+                    WriteToResults($"Error acquiring access token: {ex}");
+                }
+                catch (Exception ex)
+                {
+                    WriteToResults($"Error: {ex}");
+                }
+
+            });
+            System.Threading.Tasks.Task.Run(action);
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
